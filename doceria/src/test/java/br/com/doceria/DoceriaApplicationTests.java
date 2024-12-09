80fd1709c3ea6fde163e51bd0ef8a6bda2ecb37f
Matheus Zem <matheuslajazem@gmail.com>
2024-12-09 09:48:29 -0300

    Initializing a Spring Project

---
+++ doceria/src/test/java/br/com/doceria/DoceriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.doceria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoceriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
